22952276d6d95b249a5e68aa1c185a04df3d2df9
Quazzik <113617617+Quazzik@users.noreply.github.com>
2026-09-18 19:28:21 +0500

    added containers for web app and  slagMode api

--- SlagModeServer/Program.cs
+++ SlagModeServer/Program.cs
@@ -62,6 +62,16 @@ builder.Services.AddSwaggerGen(
     });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:8000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
